62ca4af7437a1f7afbe834aaa5d5606f2d69cf62
MexJason <jasonsdubon@gmail.com>
2023-07-30 09:01:11 -0400

    section 1.3: Fitness activity home screen UI & ActivityCard

--- FitnessApp/Home/HomeView.swift
+++ FitnessApp/Home/HomeView.swift
@@ -12,9 +12,16 @@ struct HomeView: View {
     @State var active: Int = 52
     @State var stand: Int = 8
     
+    var mockActivites = [
+        Activity(id: 0, title: "Today steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .green, amount: "9,812"),
+        Activity(id: 1, title: "Today", subtitle: "Goal 1,000", image: "figure.walk", tintColor: .red, amount: "812"),
+        Activity(id: 2, title: "Today steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .blue, amount: "9,812"),
+        Activity(id: 3, title: "Today steps", subtitle: "Goal 50,000", image: "figure.run", tintColor: .purple, amount: "55,812"),
+    ]
+    
     var body: some View {
         ScrollView(showsIndicators: false) {
-            VStack {
+            VStack(alignment: .leading) {
                 Text("Welcome")
                     .font(.largeTitle)
                     .padding()
@@ -73,8 +80,30 @@ struct HomeView: View {
                 }
                 .padding()
                 
+                HStack {
+                    Text("Fitness Activity")
+                        .font(.title2)
+                    
+                    Spacer()
+                    
+                    Button {
+                        print("show more")
+                    } label: {
+                        Text("Show more")
+                            .padding(.all, 10)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                            .cornerRadius(20)
+                    }
+                }
+                .padding(.horizontal)
                 
-                
+                LazyVGrid(columns: Array(repeating: GridItem(spacing: 20), count: 2)) {
+                    ForEach(mockActivites, id: \.id) { activity in
+                        ActivityCard(activity: activity)
+                    }
+                }
+                .padding(.horizontal)
             }
         }
     }
